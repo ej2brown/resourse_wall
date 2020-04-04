aa0486803e6e27875afbb82ba607576cc55bd8d9
Rohan Batra <rohanbatra24@gmail.com>
2020-04-04 19:51:31 +0000

    initial commit

--- routes/users.js
+++ routes/users.js
@@ -6,20 +6,52 @@
  */
 
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const { Pool } = require('pg');
+const pool = new Pool({ user: 'labber', password: 'labber', host: 'localhost', database: 'midterm' });
 
 module.exports = (db) => {
-  router.get("/", (req, res) => {
-    db.query(`SELECT * FROM users;`)
-      .then(data => {
+  router.get('/', (req, res) => {
+    db
+      .query(`SELECT * FROM users;`)
+      .then((data) => {
         const users = data.rows;
         res.json({ users });
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
   });
-  return router;
+
+  const showResults = (name) => {
+    return pool
+      .query(`SELECT * FROM widgets where name = '${input}';`)
+      .then((data) => {
+        const users = data.rows;
+        res.json({ users });
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  };
+
+  router.get('/search', (req, res) => {
+    showResults(req.query).then((results) => res.send({ results }));
+
+    return router;
+  });
 };
+
+const dbTesting = function() {
+  console.log('fn dbtetsing works');
+};
+exports.dbTesting = dbTesting;
+
+router.post('/dbTest', (req, res) => {
+  dbTesting()
+    .then(() => {
+      console.log('works');
+      res.send({ user: { name: user.name, email: user.email, id: user.id } });
+    })
+    .catch((e) => res.send(e));
+});
