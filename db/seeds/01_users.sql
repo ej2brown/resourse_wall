f5f618e76a83e7ebb25f66ae2982800667e4bd5d
ej2brown <ej2brown@edu.uwaterloo.ca>
2020-04-05 14:32:13 -0400

    created category seeds and data for resources and categories for one user

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,9 +1,15 @@
 -- Users table seeds here (Example)
-INSERT INTO users
-  (id,name,email,username,password)
-VALUES
-  (1, 'Alice', 'a@a.com', 'alice1', 'password');
-INSERT INTO users
-  (id,name,email,username,password)
-VALUES
-  (2, 'qqqq', 'v@b.com', 'dfghh1', 'password');
+INSERT INTO users (id, name, email, username, password)
+VALUES (1, 'Alice', 'alice@a.com', 'alice1', 'password');
+
+INSERT INTO users (id, name, email, username, password)
+VALUES (2, 'Ben', 'ben@b.com', 'ben2', 'password');
+
+INSERT INTO users (id, name, email, username, password)
+VALUES (3, 'Chris', 'chris@c.com', 'chris3', 'password');
+
+INSERT INTO users (id, name, email, username, password)
+VALUES (4, 'Danielle', 'danielle@c.com', 'danielle4', 'password');
+
+INSERT INTO users (id, name, email, username, password)
+VALUES (5, 'Edward', 'edward@b.com', 'Edward5', 'password');
