ba4689a648d66f3ec2eabe8b3d6f1d45cfb4e433
Rohan Batra <rohanbatra24@gmail.com>
2020-04-06 20:05:25 +0000

    finished categories route and all working

--- db/seeds/02_categories.sql
+++ db/seeds/02_categories.sql
@@ -1,11 +1,19 @@
-INSERT INTO categories (id, user_id, name) 
-VALUES (1, 1, 'food');
+INSERT INTO categories
+  (user_id, name)
+VALUES
+  (1, 'food');
 
-INSERT INTO categories (id, user_id, name) 
-VALUES (2, 1, 'coding');
+INSERT INTO categories
+  (user_id, name)
+VALUES
+  (1, 'coding');
 
-INSERT INTO categories (id, user_id, name) 
-VALUES (3, 1, 'animals');
+INSERT INTO categories
+  (user_id, name)
+VALUES
+  (1, 'animals');
 
-INSERT INTO categories (id, user_id, name) 
-VALUES (4, 1, 'memes');
+INSERT INTO categories
+  (user_id, name)
+VALUES
+  (1, 'memes');
--- routes/users.js
+++ routes/users.js
@@ -16,7 +16,6 @@ module.exports = (db) => {
       .query(`SELECT * from categories;`)
       .then((data) => {
         const categories = data.rows;
-        console.log(categories);
         res.render('categories', { categories });
       })
       .catch((err) => {
@@ -39,22 +38,56 @@ module.exports = (db) => {
     // add logic for IF logged in, otherwise display message 'please login to add resource'
 
     const input = req.body;
-    db
-      .query(
-        `INSERT INTO resources(title, description, url)
+
+    const createResource = (req, res, categoryId) => {
+      db.query(
+        `INSERT INTO resources(title, category_id,description, url)
          VALUES('${input.title}','${input.description}','${input.url}');`
-      )
-      .then(res.redirect('/'))
-      .catch((e) => res.send(e));
+      );
+    };
+
+    // 1. separate the create a new resource db query into a separate function eg, const createResource = (req, res, categoryId) => { /* copy the full db query here */ }
+    // 2. query db for category by name
+    // 3. in the .then() for that query, if the category exists, use the id on the category and create a new resource by calling createResource(req, res, categoryId)
+    // 4. if the category does not exist, make a new db insert call with the category name, and use RETURNING * at the end of your query to return the category in the then for this db call
+    // 5. in the .then() for your insert category call, create a new resource by calling createResource(req, res, categoryId)
+
+    db.query(`select * from categories where name = '${input.category}';`).then((data) => {
+      if (data.rows[0]) {
+        db
+          .query(
+            `INSERT INTO resources(title, category_id,description, url)
+                  VALUES('${input.title}','${data.rows[0].id}','${input.description}','${input.url}');`
+          )
+          .then(res.redirect('/'))
+          .catch((e) => res.send(e));
+      } else {
+        console.log('in else');
+        db
+          .query(
+            `INSERT INTO categories(user_id, name)
+                  VALUES(1,'${input.category}')
+                  RETURNING *;`
+          )
+          .then((data) => {
+            db.query(
+              `INSERT INTO resources(title, category_id,description, url)
+                  VALUES('${input.title}','${data.rows[0].id}','${input.description}','${input.url}');`
+            );
+          })
+          .then(res.redirect('/'))
+          .catch((e) => res.send(e));
+      }
+    });
   });
 
   //HOME ROUTE
   router.get('/', (req, res) => {
     //TO DO: display rescourse and liked resources
     db
-      .query(`SELECT * FROM resources;`)
+      .query(`SELECT * FROM resources join categories on categories.id = category_id;`)
       .then((data) => {
-        const resources = data.rows[0];
+        const resources = data.rows;
         res.render('index', { resources });
       })
       .catch((err) => {
@@ -66,7 +99,7 @@ module.exports = (db) => {
   router.get('/search', (req, res) => {
     const input = req.query.search;
     db
-      .query(`SELECT * FROM resources WHERE title LIKE '%${input}%';`)
+      .query(`SELECT * FROM resources join categories on categories.id = category_id WHERE title LIKE '%${input}%';`)
       .then((data) => {
         const resources = data.rows;
         res.render('search_results', { resources });
