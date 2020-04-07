6a4d8d23f65191c2d123153e3fe4ee1145bc9458
ej2brown <ej2brown@edu.uwaterloo.ca>
2020-04-07 13:42:39 -0400

    create db likes ,ratings and comments

--- db/seeds/03_resources.sql
+++ db/seeds/03_resources.sql
@@ -9,17 +9,18 @@ VALUES
         );
 
 INSERT INTO resources
-  (category_id, title, description, url)
+  (category_id, title, description, image, url)
 VALUES
   (2,
     'Coding Acdemy Website Link',
     'Learn how to code for free!',
+    'https://www.freecodecamp.org/',
     'https://www.freecodecamp.org/'
         );
 
 
 INSERT INTO resources
-  (category_id, title, description, url)
+  (category_id, title, description, image, url)
 VALUES
   (4,
     'Adorable Pets Image',
@@ -29,75 +30,83 @@ VALUES
         );
 
 INSERT INTO resources
-  (category_id, title, description, url)
+  (category_id, title, description, image, url)
 VALUES
   (4,
     'Covonavirus Meme',
     'LOL',
+    'https://www.instagram.com',
     'https://www.instagram.com/p/B9U2XDVnS-I/?utm_source=ig_embed'
         );
 
 INSERT INTO resources
-  (category_id, title, description, url)
+  (category_id, title, description, image, url)
 VALUES
   (1,
     'Hamburger Image',
     'Hamburger with french fries and a beer',
+    'https://upload.wikimedia.org',
     'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/RedDot_Burger.jpg/1200px-RedDot_Burger.jpg'
         );
 
 INSERT INTO resources
-  (category_id, title, description, url)
+  (category_id, title, description, image, url)
 VALUES
   (4,
     'Dog Meme',
     '"Sir, lower your voice"',
+    'https://hips.hearstapps.com/',
     'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-desk-meme-1546548760.png?crop=0.515xw:1.00xh;0.0466xw,0&resize=480:*'
         );
 
 INSERT INTO resources
-  (category_id, title, description, url)
+  (category_id, title, description, image, url)
 VALUES
   (4,
     '2020 Meme',
     'Flexing in March 2020',
+    'https://img.buzzfeed.com/',
     'https://img.buzzfeed.com/buzzfeed-static/static/2020-03/27/20/campaign_images/71a6dce052c4/17-funny-coronavirus-memes-that-gave-me-some-reli-2-197-1585340939-0_dblbig.jpg'
         );
 
 
 INSERT INTO resources
-  (category_id, title, description, url)
+  (category_id, title, description, image, url)
 VALUES
   (4,
     '2020 Meme',
     'Flexing in March 2020',
+    'https://img.buzzfeed.com/',
     'https://img.buzzfeed.com/buzzfeed-static/static/2020-03/27/20/campaign_images/71a6dce052c4/17-funny-coronavirus-memes-that-gave-me-some-reli-2-197-1585340939-0_dblbig.jpg'
         );
 
 INSERT INTO resources
-  (category_id, title, description, url)
+  (category_id, title, description, image, url)
 VALUES
   (1,
     'Salmon Dinner Recipe',
     'link to a salmon recipe for dinner',
+    'https://www.foodnetwork.com',
     'https://www.foodnetwork.com/recipes/bobby-flay/salmon-with-brown-sugar-and-mustard-glaze-recipe-1926380'
         );
 
 INSERT INTO resources
-  (category_id, title, description, url)
+  (category_id, title, description, image, url)
 VALUES
   (1,
     'Veggies Image',
     'Inspiration to eat veggies',
+    'https://www.jennycraig.com',
     'https://www.jennycraig.com.au/wp-content/uploads/wpfiles/2019/05/20190604-veggies.jpg'
         );
 
 
 INSERT INTO resources
-  (category_id, title, description, url)
+  (category_id, title, description, image, url)
 VALUES
   (5,
     'Karate',
     'The top 10 karate classes in Toronto',
+    'https://www.blogto.com',
     'https://www.blogto.com/sports_play/2014/11/the_top_10_karate_classes_in_toronto/'
   );
