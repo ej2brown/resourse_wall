47b63facaec3c09f02c65bf0b33c0a14df26e5c4
ej2brown <ej2brown@edu.uwaterloo.ca>
2020-04-08 10:51:07 -0400

    create tables for comments and and ratings

--- db/schema/06_comments.sql
+++ db/schema/06_comments.sql
@@ -3,5 +3,6 @@ CREATE TABLE comments
 (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  resource_id INTEGER REFERENCES resources(id) ON DELETE CASCADE
+  resource_id INTEGER REFERENCES resources(id) ON DELETE CASCADE,
+  comment VARCHAR(255) NOT NULL
 );
---
+++ db/seeds/06_comments.sql
@@ -0,0 +1,2 @@
+INSERT INTO comments(user_id, resource_id, comment)
+	VALUES (1, 1, 'test');
