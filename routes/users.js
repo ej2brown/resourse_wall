cd87df8e3826a359ddcb2d20c3709d2e25adacf1
Rohan Batra <rohanbatra24@gmail.com>
2020-04-09 15:59:52 +0000

    profile page changed

--- routes/users.js
+++ routes/users.js
@@ -110,10 +110,15 @@ module.exports = (db) => {
 
   //PROFILE route GET
   router.get('/profile', (req, res) => {
+    if (!req.session.email) {
+      res.render('login');
+      return;
+    }
+
     db
       .query(`SELECT * FROM users WHERE users.email = '${req.session.email}';`)
       .then((data) => {
-        const user = data.rows;
+        const user = data.rows[0];
         // console.log('BOOP', user);
         res.render('profile', { user });
       })
@@ -126,22 +131,23 @@ module.exports = (db) => {
   //PROFILE route POST
   router.post('/profile', (req, res) => {
     //TO DO: form for edit then return to profile page
-    const option = req.body.edit;
-    const field = '';
-    if (req.body[edit] === name) field = name;
-    if (req.body[edit] === username) field = username;
-    if (req.body[edit] === email) field = email;
-    db
-      .query(`UPDATE users SET $1 = $2 WHERE users.id = 1;`, [ field, option ])
-      .then((data) => {
-        const user = data.rows[0];
-        // console.log('=====', user);
-        return res.redirect('/profile', { user }); //assuming edit.ejs
-        /*note: ejs file would need user.name, user.username, user.email and profile pic */
-      })
-      .catch((err) => {
-        res.status(500).json({ error: err.message });
-      });
+    // const option = req.body.edit;
+    // console.log(req.body.edit);
+    // const field = '';
+    // if (req.body[edit] === name) field = name;
+    // if (req.body[edit] === username) field = username;
+    // if (req.body[edit] === email) field = email;
+    // db
+    //   .query(`UPDATE users SET $1 = $2 WHERE users.id = 1;`, [ field, option ])
+    //   .then((data) => {
+    //     const user = data.rows[0];
+    //     // console.log('=====', user);
+    //     return res.redirect('/profile', { user }); //assuming edit.ejs
+    //     /*note: ejs file would need user.name, user.username, user.email and profile pic */
+    //   })
+    //   .catch((err) => {
+    //     res.status(500).json({ error: err.message });
+    //   });
   });
 
   return router;
